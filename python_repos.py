68393af8a1126b7e7b762ff5981f23264d87ae04
Jake Sweger <jsweg13@gmail.com>
2023-11-20 15:26:51 -0500

    add repo_names and stars dicts

--- python_repos.py
+++ python_repos.py
@@ -9,13 +9,10 @@ headers = {'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
 
-# Store API response in a variable
+# Process results
 response_dict = r.json()
-print(f"Total repositories: {response_dict['total_count']}")
-
-# Explore information about the repositories.
 repo_dicts = response_dict['items']
-print(f"Repositories returned: {len(repo_dicts)}")
+repo_names, stars = [], []
 
 # Explore the first repository
 repo_dict = repo_dicts[0]
