119b8c2490b7dcc88c632bf8a6bef8e70179049a
Jake S <jsweg13@gmail.com>
2023-12-14 15:43:18 -0500

    Add api cal

--- hn_submissions.py
+++ hn_submissions.py
@@ -1,2 +1,7 @@
 from operator import itemgetter
 import requests
+
+# Make an API Call and store the response
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+r = requests.get(url)
+print(f"Status code: {r.status_code}")
