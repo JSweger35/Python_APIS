e15aa18e7646d82870574911a2375602c2937111
Jake Sweger <jsweg13@gmail.com>
2023-12-15 15:55:49 -0500

    fix merge

--- hn_submissions.py
+++ hn_submissions.py
@@ -1,13 +1,8 @@
 from operator import itemgetter
-<<<<<<< HEAD
 
-import requests
-#Make an API call and store the response
-=======
 import requests
 
 # Make an API Call and store the response
->>>>>>> 119b8c2490b7dcc88c632bf8a6bef8e70179049a
 url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
 r = requests.get(url)
 print(f"Status code: {r.status_code}")
