52dc6162f13a5b67886b7662091634950a37dc48
Jake Sweger <jsweg13@gmail.com>
2023-12-15 15:53:43 -0500

    change API call

--- hn_submissions.py
+++ hn_submissions.py
@@ -2,6 +2,6 @@ from operator import itemgetter
 
 import requests
 #Make an API call and store the response
-url = 'https://hacker-news.firebaseio.com/v0/item/19155826.json'
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
 r = requests.get(url)
 print(f"Status code: {r.status_code}")
