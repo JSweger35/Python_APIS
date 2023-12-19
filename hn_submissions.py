4c7abbcbc8a44e04e230f438c6d43e1016f23bd0
Jake Sweger <jsweg13@gmail.com>
2023-12-19 15:50:04 -0500

    set up api call for each submission

--- hn_submissions.py
+++ hn_submissions.py
@@ -11,4 +11,8 @@ print(f"Status code: {r.status_code}")
 submission_ids = r.json()
 submission_dicts = []
 for submission_id in submission_ids[:30]:
-    
+    # Make seperate API calls for each submission
+    url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
+    r = requests.get(url)
+    print(f"Status code: {r.status_code}")
+    response_dict = r.json()
